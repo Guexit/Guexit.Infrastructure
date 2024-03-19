030fb49ea17792da7f1cf6eb65837e1c4d6693fe
Manuel Mínguez <manuelinf12@gmail.com>
2024-03-19 17:58:00 +0100

    Removing interpolation with dollar

--- providers.tf
+++ providers.tf
@@ -10,10 +10,10 @@ terraform {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "3.89.0"
-      subscription_id   = ${var.azure_subscription_id}
-      tenant_id         = ${var.azure_subscription_tenant_id}
-      client_id         = ${var.azure_client_id}
-      client_secret     = ${var.azure_subscription_client_secret}
+      subscription_id   = var.azure_subscription_id
+      tenant_id         = var.azure_subscription_tenant_id
+      client_id         = var.azure_client_id
+      client_secret     = var.azure_subscription_client_secret
     }
     random = {
       source  = "hashicorp/random"
